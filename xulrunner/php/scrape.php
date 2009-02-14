df8bc3a3b72adb06a9a7ab5dbc26e55373fbc3b6
Alf <sites@hubmed.org>
2009-02-14 22:06:45 +0000

    moving into xulrunner folder

--- xulrunner/php/scrape.php
+++ xulrunner/php/scrape.php
@@ -1,15 +1,12 @@
 #!/usr/bin/env php
 <?php
 
-//require __DIR__ . '/../../main.inc.php';
 $dir = __DIR__ . '/../definitions';
 
 $files = scandir($dir);
 foreach ($files as $file){
   if (!preg_match('/\.js$/', $file))
     continue;
-    
-  //debug($file);
   
   $json = file_get_contents($dir . '/' . $file);
   $json = str_replace('\\', '\\\\', $json); // for regular expressions
@@ -25,12 +22,12 @@ foreach ($files as $file){
     'url' => $defs->url,
     'defs' => json_encode($defs),
     );
-
+  
   //$eccstract = 'http://0.0.0.0:10000?' . http_build_query($params); // does urlencode instead of rawurlencode, adds unwanted plusses
   $url = sprintf('http://0.0.0.0:10000?url=%s&defs=%s', urlencode($params['url']), rawurlencode($params['defs'])); // better to use POST?
   //debug($url);
-
   $json = file_get_contents($url);
+  
   print $json; //debug($json);
 
   $items = json_decode($json);
